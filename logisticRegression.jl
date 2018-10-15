c54aa27d03c6130db3f22b95ba953659a60509ac
GUO ZHENGYANG <41659250+GZY219@users.noreply.github.com>
2018-10-15 12:24:32 +0800

    Update logisticRegression.jl

--- logisticRegression.jl
+++ logisticRegression.jl
@@ -1,6 +1,6 @@
 using PyCall
 using ArgParse
-# This module allows the creation of user-friendly command-line interfaces to Julia programs: the program defines which arguments, options and sub-commands it accepts, and the ArgParse module does the actual parsing, issues errors when the input is invalid, and automatically generates help and usage messages.
+# the program defines which arguments, options and sub-commands it accepts.
 
 include("dataUtils.jl")
 
